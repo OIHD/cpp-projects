ce556f80f7d31b58c79bf39b6c79e7e6572f4745
oihd <oihdofficial@outlook.com>
2023-03-12 13:46:36 +0300

    yilan oyunu tamamlandi

--- snakegame/src/Oyna.cpp
+++ snakegame/src/Oyna.cpp
@@ -2,7 +2,7 @@
 
 const int genislik = 20 , yukseklik = 20 ;
 int indexakonumyatay = genislik/2 , indexakonumdikey = yukseklik/2 , asayac = 0 , yilanboyutu = 1
-, turbekle = 0 , haritadaelmasayisi=0 , elmax=0 , elmay=0 , 
+, turbekle = 0 , haritadaelmasayisi=0 , elmax=0 , elmay=0 , cakisma=0 , elmaskor=0 , oyunhizi ,
 gamespeed = 1;
 bool baslangic = 1, oyundevam = 1 , blokkoyuldu = 0;
 char sontus = 'w';
@@ -13,11 +13,21 @@ void Oyna(char* input )
 {
 while(oyundevam){ ++asayac;
 srand(time(nullptr));
+oyunhizi = (gamespeed*110000)-((elmaskor+1)*5000);
             if (haritadaelmasayisi==0)
             {
+                ++elmaskor;
                 ++haritadaelmasayisi ;
                 elmax = 1+(rand() % (genislik-1)); 
                 elmay = 1+(rand() % (yukseklik-1));
+                for (size_t i = 0; i < yilanboyutu; i++)
+                {
+                    if (elmax == yilanxkonum[i]&&elmay==yilanykonum[i])
+                    {
+                        elmax = 1+(rand() % (genislik-1)); 
+                        elmay = 1+(rand() % (yukseklik-1));
+                    } 
+                }                
             }
             if (elmax == indexakonumyatay && elmay==indexakonumdikey)
             {
@@ -32,13 +42,19 @@ srand(time(nullptr));
                 yilanxkonum[asayac] = indexakonumyatay;
                 yilanykonum[asayac] = indexakonumdikey;
 
-            for (size_t i = 0; i < yilanboyutu; i++)
+            for (size_t k = 0; k < yilanboyutu; k++)
             {
-                if (yilanxkonum[asayac+1] == indexakonumyatay && yilanykonum[asayac+1] == indexakonumdikey )
+                for (size_t j = 0; j < yilanboyutu; j++)
                 {
-                    oyundevam = 0; // bu kısım düzenlenecek yılanın kendine çarpması mekaniği eklenecek 
+                    if(yilanxkonum[j] == yilanxkonum[k] && yilanykonum[j] == yilanykonum[k])
+                    ++cakisma;
+                    if ( cakisma >= (yilanboyutu+1) )
+                    {
+                    oyundevam = 0;
+                    }
                 }   
             }
+            cakisma = 0 ;
 
 for (size_t yuksekligi = 1; yuksekligi < yukseklik; yuksekligi++)
     {
@@ -89,14 +105,17 @@ switch (*input)
                         }
 if (indexakonumyatay<=0||indexakonumyatay>=genislik||indexakonumdikey<=0||indexakonumdikey>=yukseklik)
     oyundevam = 0;
-std::cout << "\n" << "son harf girdisi : "<< *input << "\n";
-std::cout << "\n" << "konum X : "<< indexakonumyatay << " konum Y : "<< indexakonumdikey ;
-std::cout << "\n" << "Toplam Donguler : "<< asayac << "\n";
-std::cout <<  "1x :"<< yilanxkonum[1] << " 1y :" << yilanykonum[1] ;
-std::cout <<  " |2x :"<< yilanxkonum[2] << " 2y: " << yilanykonum[2] ;
-std::cout <<  " |3x :"<< yilanxkonum[3] << " 3y: " << yilanykonum[3] << "\n";
-std::cout << "\n" << "ex : "<< elmax << "ey : " << elmay << "\n";
-usleep(gamespeed*90000);
+std::cout << "\n" << "- Oyun Başladı , Toplam skor : "<< elmaskor*100 <<" -"<< "\n";
+usleep(oyunhizi);
 system("clear");}//WHİLE
-std::cout << "---------------------------- OYUN BITTI ----------------------------"<<std::endl ;
+for (size_t yuksekligi = 1; yuksekligi < yukseklik; yuksekligi++)
+    {
+    std::cout << "\n" ; 
+    for (size_t genisligi = 1; genisligi < genislik; genisligi++)
+        {
+                        std::cout << "▒▒" ; 
+        }
+    }
+    std::cout << "\n" << "- Toplam skor " << elmaskor*100 <<" -"<< "\n";
+    std::cout << "\n" << "- cikmak icin 2 kere 'q' ya basın -"<< "\n";
 }
